9ce0ace7145ead88f5ace43ddb106b2b04ce99d5
YuTakaki <yu.takaki2000@gmail.com>
2020-09-25 23:49:03 +0900

    Updates

---
+++ precache-manifest.add41cf17e4ba332ea48a0132e5ace4f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347f2aef034235966bacafa8b394a446",
+    "url": "/popcorn-time/index.html"
+  },
+  {
+    "revision": "d483f9ec6001a728d5f8",
+    "url": "/popcorn-time/static/css/3.6172b2e3.chunk.css"
+  },
+  {
+    "revision": "295da8551629c0c4d813",
+    "url": "/popcorn-time/static/css/main.ad993d4d.chunk.css"
+  },
+  {
+    "revision": "139f0fe92114e67ff9b6",
+    "url": "/popcorn-time/static/js/2.11e5c1bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/popcorn-time/static/js/2.11e5c1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d483f9ec6001a728d5f8",
+    "url": "/popcorn-time/static/js/3.2e84c072.chunk.js"
+  },
+  {
+    "revision": "295da8551629c0c4d813",
+    "url": "/popcorn-time/static/js/main.2d3fdb61.chunk.js"
+  },
+  {
+    "revision": "c3328e216f47c9602a2d",
+    "url": "/popcorn-time/static/js/runtime-main.62709e4a.js"
+  },
+  {
+    "revision": "f0e79c237eba7629393f31a151ac7ca0",
+    "url": "/popcorn-time/static/media/heart.f0e79c23.png"
+  },
+  {
+    "revision": "5bd58f1ac6b6c347a6004fb44239417f",
+    "url": "/popcorn-time/static/media/logo.5bd58f1a.png"
+  },
+  {
+    "revision": "b1edc35f0fa63d0e0b3c9bc5537de942",
+    "url": "/popcorn-time/static/media/noImage.b1edc35f.png"
+  },
+  {
+    "revision": "5fe49163889ef5ffa7cc1294229c38a2",
+    "url": "/popcorn-time/static/media/star.5fe49163.png"
+  },
+  {
+    "revision": "9b0f52ad060ca747a4643c99444b7392",
+    "url": "/popcorn-time/static/media/star2.9b0f52ad.png"
+  }
+]);
